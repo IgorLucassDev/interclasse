1fe3c4ed30a66ae8e7883efe95656b67619a995c
IgorLucasDev <62673756+IgorLucassDev@users.noreply.github.com>
2024-08-30 19:09:02 -0300

    login and register page

--- client/components/navbar/navbar.php
+++ client/components/navbar/navbar.php
@@ -4,8 +4,9 @@
 <nav class="navbar">
         <h1 class="poppins-bold">LOGO</h1>
         <ul class="list">
-            <li><a href="#">Home</a></li>
+            <li><a href="#">Inicio</a></li>
             <li><a href="#">Login</a></li>
+            <li><a href="#">Cadastro</a></li>
         </ul>
 </nav>
 
--- client/pages/cadastro/cadastro.php
+++ client/pages/cadastro/cadastro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="login.css?v=1.0"">
+    <link rel="stylesheet" href="cadastro.css?v=1.0"">
     <link rel="stylesheet" href="../../global.css">
     <!-- Navbar CSS link -->
     <link rel="stylesheet" href="../../components/navbar/navbar.css">
@@ -23,9 +23,31 @@
                 <label for="password">Senha</label>
                 <input type="password" id="password" name="password" placeholder="Digite sua senha" required>
             </div>
-            <a class="cad" href="#">Não tenho uma conta</a>
+            <div class="user-input poppins-extrabold">
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" name="nome" placeholder="Digite seu nome completo" required>
+            </div>
+            <div class="user-input poppins-extrabold">
+                <label for="turma">Turma</label>
+                <input type="number" id="turma" name="turma" placeholder="Ex: 201" required>
+            </div>
+            <div class="tipo poppins-extrabold">
+                <div class="option">
+                    <label for="tipo">Aluno</label>
+                    <input type="radio" name="tipo" id="tipo">
+
+                </div>
+                <div class="option">
+                    <label for="tipo">Professor</label>
+                    <input type="radio" name="tipo" id="tipo">
+
+                </div>
+            </div>
+
+
+            
             <div class="button-container">
-                <button type="submit">Entrar</button>
+                <button type="submit">Registrar</button>
             </div>
         </form>
     </div>

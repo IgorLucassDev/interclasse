c845eed13bb4e1f5870f0fca643e57f9c4470cc8
IgorLucasDev <62673756+IgorLucassDev@users.noreply.github.com>
2024-08-30 18:05:23 -0300

    Project cretion

---
+++ client/components/navbar/navbar.php
@@ -0,0 +1,12 @@
+
+<!-- Navbar -->
+
+<nav class="navbar">
+        <h1 class="poppins-bold">LOGO</h1>
+        <ul class="list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+</nav>
+
+<!-- End Navbar -->
---
+++ client/pages/login/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="../../global.css">
+    <!-- Navbar CSS link -->
+    <link rel="stylesheet" href="../../components/navbar/navbar.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <?php include '../../components/navbar/navbar.php'; ?> 
+
+    <div class="container">
+        <form action="" method="post" class="login-form">
+            <div class="user-input">
+                
+            </div>
+        </form>
+    </div>
+</body>
+</html>
